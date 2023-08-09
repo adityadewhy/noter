f59f5cd8a2aa5c236f6917f6eefcb6fdf4131bd5
aditya <adityadewhy@gmail.com>
2023-08-09 20:26:15 +0530

    firebase setup done

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL : "https://noter-4f730-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const noteItemsDB = ref(database, "noteItems")
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
+    push(noteItemsDB, inputValue)
     console.log(inputValue)
 })
